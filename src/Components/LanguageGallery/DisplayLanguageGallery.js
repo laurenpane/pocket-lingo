da17442ae38708a8ed61c04a1dee9e6151d8684b
Lauren Pane <lauren.pane1@googlemail.com>
2020-08-25 15:30:01 +0100

    Downloaded react-router-dom

--- src/Components/LanguageGallery/DisplayLanguageGallery.js
+++ src/Components/LanguageGallery/DisplayLanguageGallery.js
@@ -6,12 +6,14 @@ export default function DisplayLanguageGallery(props) {
   return (
     // <Link to={`/${props.id}`} className="lang">
     <>
-      <img
-        src={props.image}
-        alt={props.name}
-        className={`${props.name}-flag`}
-        id={props.name}
-      />
+      <div className="item">
+        <img
+          src={props.image}
+          alt={props.name}
+          className="flag"
+          id={props.name}
+        />
+      </div>
       <div className="overlay">
         <h2 className="showName">{props.name}</h2>
       </div>
