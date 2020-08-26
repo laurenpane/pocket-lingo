13b16038fef2475c4426a7335cd2aa4dd0b4738a
Lauren Pane <lauren.pane1@googlemail.com>
2020-08-26 10:38:24 +0100

    Added overlay to flags

--- src/Components/LanguageGallery/DisplayLanguageGallery.js
+++ src/Components/LanguageGallery/DisplayLanguageGallery.js
@@ -5,17 +5,30 @@ import "./LanguageGallery.css";
 export default function DisplayLanguageGallery(props) {
   return (
     <Link to={`/${props.id}`} className="lang">
-      <div className="item">
-        <img
-          src={props.image}
-          alt={props.name}
-          className="flag"
-          id={props.name}
-        />
-      </div>
+      <img
+        src={props.image}
+        alt={props.name}
+        className="flag"
+        id={props.name}
+      />
       <div className="overlay">
-        <h2 className="showName">{props.name}</h2>
+        <h2 className="text">{props.name}</h2>
       </div>
     </Link>
   );
 }
+
+// export default function Series(props) {
+//   return (
+//     <Link to={`/${props.id}`} className="item">
+//       <img src={props.logo} alt={props.name} id="showLogo" />
+//       <div className="overlay">
+//         <div className="text">
+//           <h2 id="showName">{props.name}</h2>
+//           <p className="description">{props.description}</p>
+//           <p className="click">{props.click}</p>
+//         </div>
+//       </div>
+//     </Link>
+//   );
+// }
