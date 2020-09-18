7712336d27715e632e28954e16cfa0f64c4ebd19
Lauren Pane <lauren.pane1@googlemail.com>
2020-09-18 10:44:54 +0100

    Data loading on first refresh but none thereafter

--- src/setUpProxy.js
+++ src/setUpProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/rest/questions",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "http://[::1]:3000",
       changeOrigin: true,
     })
   );
