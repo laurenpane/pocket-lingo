7cfb1ce2c08b44106b31900b19d39bb7e61073b5
Lauren Pane <lauren.pane1@googlemail.com>
2020-09-23 16:28:35 +0100

    Initial English API setup attempt

--- src/Components/English/English.js
+++ src/Components/English/English.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./English.css";
 
 export default function English() {
+  const [word, setWord] = useState(undefined);
+  const API_FULL_URL = `${process.env.API_URL}${process.env.API_KEY}`;
+  useEffect(() => {
+    async function getRandomMeal() {
+      const response = await fetch(API_FULL_URL);
+      const data = await response.json();
+      setWord(data);
+      console.log(word);
+    }
+    getRandomMeal();
+  }, []);
   return <div className="english">English</div>;
 }
